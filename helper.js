f40aa794b6abbca965fe009d10279efe913babf6
sasuke7779 <sasuke7779@gmail.com>
2022-06-23 23:02:05 +0900

    0623 sample

--- helper.js
+++ helper.js
@@ -1,3 +1,23 @@
-const name = 'Mike';
+const fs = require("fs");
 
-module.exports = name;
+let person = {
+  name: "Mike",
+  age: 30,
+};
+
+const read = function () {
+  fs.readFile("./sample.json", "utf8", function (err, data) {
+    const person = JSON.parse(data);
+    console.log(person.age);
+  });
+};
+const write = function () {
+  fs.writeFile("sample.json", JSON.stringify(person), function () {
+    console.log("書き出し完了");
+  });
+};
+
+module.exports = {
+  read: read,
+  write: write,
+};
--- sample.js
+++ sample.js
@@ -1,3 +1,10 @@
-const name = require('./helper');
+const { read, write } = require("./helper");
 
-console.log(name);
+const request = process.argv[2];
+if (request === "read") {
+  read();
+} else if (request === "write") {
+  write();
+} else {
+  console.error("エラー : readまたはwriteの引数を入れてください");
+}
