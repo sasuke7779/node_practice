68a86ea7355c9cee578fc418c97ad3bcd5497290
sasuke7779 <sasuke7779@gmail.com>
2022-06-25 21:39:40 +0900

    make form

--- web-server/index.js
+++ web-server/index.js
@@ -2,9 +2,25 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+app.use(express.urlencoded({ extended: false })); //req.bodyの中身を使えるようにするおまじない
 app.use(express.static(path.join(__dirname, "public")));
 
 // app.get("/", function (req, res) {});
+
+app.post("/api/v1/quiz", function (req, res) {
+  const answer = req.body.answer;
+  if (answer === "2") {
+    // HTMLで返すパターン1（res.sendでテキストを返す）
+    // res.send("<h1>正解</h1>");
+    // HTMLで返すパターン1（res.sendでリダイレクト）
+    res.redirect("/correct.html");
+  } else {
+    // HTMLで返すパターン1（res.sendで返す）
+    // res.send("<h1>不正解</h1>");
+    // HTMLで返すパターン1（res.sendでリダイレクト）
+    res.redirect("/wrong.html");
+  }
+});
 
 app.get("/api/v1/user", function (req, res) {
   res.send("About");
